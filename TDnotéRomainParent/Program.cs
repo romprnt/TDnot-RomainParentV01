927181a1f9abf5b91a693472dacb51eb35d81ac5
Romain <romain.parent18@gmail.com>
2025-05-26 15:29:25 +0200

    Ajoutez des fichiers projet.

---
+++ TDnotéRomainParent/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int saisieSexes()
+{
+    string sexeSaisie ;
+    do
+    {
+       // récupération de la saisie de l'utilisateur
+        sexeSaisie = Console.ReadLine();
+
+        // controle de saisie 
+        if (sexeSaisie != "Yo" && sexeSaisie != "Hu" && sexeSaisie != "Sk")
+        {
+            Console.WriteLine("Erreur: le sexe doit être Yo, Hu ou Sk");
+        }
+    }
+    while (sexeSaisie != "Yo" && sexeSaisie != "Hu" && sexeSaisie != "Sk");
+    // retour du sexe récupéré différent selon le sexe
+
+    if (sexeSaisie == "Yo") return 1;
+    if (sexeSaisie == "Hu") return 2;
+    return 3;
+}
+
+
