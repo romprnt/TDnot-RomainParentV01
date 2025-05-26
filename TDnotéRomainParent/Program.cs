799dff5a76f003198acb7895335a2183e54fcf6d
Romain <romain.parent18@gmail.com>
2025-05-26 16:11:01 +0200

    ajout de la procédure principale

--- TDnotéRomainParent/Program.cs
+++ TDnotéRomainParent/Program.cs
@@ -1,13 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 int saisieSexes()
 {
-    string sexeSaisie ;
+    string sexeSaisie;
 
     do
     {
         // récupération de la saisie de l'utilisateur
         Console.WriteLine("Saisissez votre sexe au format Sk/Yo/Hu ");
-        sexeSaisie = Console.ReadLine();
+        var input = Console.ReadLine();
+        sexeSaisie = input ?? ""; // Gère le cas où la saisie est nulle
 
         // controle de saisie 
         if (sexeSaisie != "Yo" && sexeSaisie != "Hu" && sexeSaisie != "Sk")
@@ -27,17 +28,50 @@ int saisieAge()
 {
     int saisieAge;
 
-        Console.WriteLine("Saisissez un age ");
-        saisieAge = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Saisissez un age ");
+    var input = Console.ReadLine();
+    if (!int.TryParse(input, out saisieAge))
+        throw new ArgumentException("Veuillez saisir un nombre entier valide pour l'âge.");
     // controle des saisie positives  
-     if (saisieAge < 0)
+    if (saisieAge < 0)
         throw new ArgumentException("Age invalide car inférieure à zéro");
 
+    return saisieAge;
+}
 
-    
+// démarrage
+int sexeValeur;
+int age;
 
-   return saisieAge;
+// appel des méthodes 
+sexeValeur = saisieSexes();
+try
+{
+    age = saisieAge();
+    Console.WriteLine(age);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message); // gère les erreurs
+    return;
+}
 
+if (sexeValeur == 1 || age > 20)
+{
+    Console.WriteLine("Cet Oxien paie l'impot ");
+}
+if (sexeValeur == 1 || age > 20)
+{
+    Console.WriteLine("Cet Oxien paie l'impot ");
+}
+if (sexeValeur == 1 || age > 20)
+{
+    Console.WriteLine("Cet Oxien paie l'impot ");
+}
+
+else
+{
+    Console.WriteLine("Cet Oxien ne paie pas l'impot ");
 }
 
 
